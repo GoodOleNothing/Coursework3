1157d6d1b6305f9e3d461a91fd98c9995f8378cf
GoodOleNothing <alexgog77777@gmail.com>
2023-02-13 20:37:47 +0500

    first try at testing

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import json
 import requests
-from datetime import datetime
 from Class import *
 
-response = requests.get('https://api.npoint.io/e6729ddf2b5e8c56e84a')
+response = requests.get('https://api.npoint.io/e6729ddf2b5e8c56e84a')#Берём данные с внешнего ресурса
 operations = json.loads(response.text)
-#print(operations)
 
-datetest = datetime.now()
-format = "%d.%m.%Y"
-print(datetest.strftime(format))
-
-d = Demonstration(operations)
+d = Demonstration(operations) #Создаём экземпляры класса
 d.looking_for_last_five_executed()
 d.collect_and_present_data()
 
